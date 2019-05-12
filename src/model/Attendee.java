614e6fc4b5d71c944d790b9f1f9aef128ab0edb5
Daniel Fernandez <alejo.dafr@gmail.com>
2019-05-12 13:16:38 -0500

    Test data generated

---
+++ src/model/Attendee.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Attendee {
+	
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String gender;
+	private String photo;
+	private Date birthday;
+	
+	public Attendee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
